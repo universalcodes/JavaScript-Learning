0f410de953b4a42b73aab046169cf5510394838f
universalcodes <yogeshkamra25@gmail.com>
2026-05-05 09:36:56 +0530

    Day 4 - Arithmetic Operator is Completed

---
+++ Day3 - Operators/AArtihmeticOperators.js
@@ -0,0 +1,59 @@
+console.log('Welcome to JavaScript Operators');
+console.log('There are different types of operators in JavaScript');
+
+
+//for Arithmetic operator - please refer AArtihmeticOperators.js
+console.log('1. Arithmetic Operators: It is used to perform Arithmetic Operation such as Sum, subtract, product, Modulus,Divide 1. Arithmetic Operators (+, -, *, /, %, ++, --, **)');
+console.log('This is Addition Operator Type of Arithmetic Operator');
+var a = 10;
+var b = 23;
+var sum = a + b;
+console.log('The sum of a and b is ', sum);
+
+console.log('This is Subtract Operator Type of Arithmetic Operator');
+var a1 = 10;
+var b1 = 23;
+var subtract = b1-a1;
+console.log('The subtract of a and b is ', subtract);
+
+console.log('This is Product or Multiply Operator Type of Arithmetic Operator');
+var c2 = 10;
+var d2 = 23;
+var multiply = c2*d2;
+console.log('The product of c2 and d2 is ', multiply);
+
+
+console.log('This is Divide Operator Type of Arithmetic Operator');
+var c3 = 10;
+var d3 = 2;
+var divideResult = c3/d3;
+console.log('The divide result of c3 and d3 is ', divideResult);
+
+console.log('This is Modulus Operator helps to find Remainder Type of Arithmetic Operator');
+var c4 = 10;
+var d4 = 3;
+var moduloResult = c4%d4;
+
+
+console.log('This is Expotentional Operator helps to get power of given number');
+var d5 = 5**5 // 5* 5* 5* 5* 5
+console.log('The Result of Expotentional Operator as follows', d5)
+
+
+console.log('The divide result of c4 and d4 is ', moduloResult);
+console.log('2. Assignment Operators:');
+console.log('3. Logical Operators');
+console.log('4. Bitwise Operators');
+console.log('5. Ternary Operator');
+console.log('6. Typeof Operator');
+console.log('7. Unary Operators');
+console.log('8. Relational Operators');
+console.log('9. Equality Operators');
+console.log('10. String Operators');
+console.log('11. Conditional Operators');
+console.log('12. Spread Operator');
+console.log('13. Destructuring Operator');
+console.log('14. Comma Operator');
+console.log('15. Exponential Operator');
+console.log('16. Increment and Decrement Operators');
+console.log('17. Shift Operators (Left, Right, Unsigned Right)');
---
+++ Day3 - Operators/BAssignmentOperator.js
@@ -0,0 +1,21 @@
+console.log('Welcome to JavaScript Operators');
+console.log('There are different types of operators in JavaScript');
+
+//for Arithmetic operator - please refer AArtihmeticOperators.js
+console.log('1. Arithmetic Operators: It is used to perform Arithmetic Operation such as Sum, subtract, product, Modulus,Divide');
+console.log('2. Assignment Operators');
+console.log('3. Logical Operators');
+console.log('4. Bitwise Operators');
+console.log('5. Ternary Operator');
+console.log('6. Typeof Operator');
+console.log('7. Unary Operators');
+console.log('8. Relational Operators');
+console.log('9. Equality Operators');
+console.log('10. String Operators');
+console.log('11. Conditional Operators');
+console.log('12. Spread Operator');
+console.log('13. Destructuring Operator');
+console.log('14. Comma Operator');
+console.log('15. Exponential Operator');
+console.log('16. Increment and Decrement Operators');
+console.log('17. Shift Operators (Left, Right, Unsigned Right)');
