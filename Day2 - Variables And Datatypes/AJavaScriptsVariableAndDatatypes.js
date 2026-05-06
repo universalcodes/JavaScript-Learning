d57af2a8c439a7bbad6f36f1066442d5d4281ea2
universalcodes <yogeshkamra25@gmail.com>
2026-05-06 09:52:15 +0530

    Day 5 - Updated Variables and Assigmnent Operator

--- Day2 - Variables And Datatypes/AJavaScriptsVariableAndDatatypes.js
+++ Day2 - Variables And Datatypes/AJavaScriptsVariableAndDatatypes.js
@@ -13,46 +13,202 @@ for example
 var booleanVariable = true;
 document.write("The Value of boolean Variable as follows ", booleanVariable);
 console.log("Printing the value of boolean Variable within the browser console as follows", booleanVariable);
----- typeof used here -- 
+
+------------------------------------- typeof used here --------------------------- 
 
 document.write("The Value of boolean Variable as follows ", typeof(booleanVariable));
 console.log("Printing the value of boolean Variable within the browser console as follows", typeof(booleanVariable));
+ 
+---------------- const----------------------------------------------
+const is the Variable where once we assign the value we cant change it
+const fixedValue = 89;
+console.log('The value of const fixedValue as follows', fixedValue);
+fixedValue = 90; // we have tried to re-assign value to fixedValue as 90
+console.log('The updated value of const fixedValue as follows', fixedValue);  // it throw error as TypeError: Assignment to constant variable.at Object.<anonymous> (F:\JavaScript Learning\Day2 - Variables And Datatypes\AJavaScriptsVariableAndDatatypes.js:65:12)
+--------------------------------------------------------------------------------------------------------
+
+
+--------------------------------------let --------------------------------------
+1. let Variable cant be reclared
+let relcarelet = 10;
+let relcarelet = 20; // ❌ Error: Identifier 'relcarelet' has already been declared
+console.log(relcarelet);  // The output is SyntaxError: Identifier 'relcarelet' has already been declared
+
+but ✅ Allowed (it is allowed in block scope)
+let c = 10;
+{
+  let c = 20; // ✅ Allowed (it is allowed in block scope)
+  console.log(c); // Output: 20
+}
+console.log(c); // Output: 10
+
+
+2. let is Variable which cant be accessed outside the function block
+for example --- Valid Let is defined inside the function
+function testLet() {
+  if (true) {
+    let y = 20;
+      console.log(y); // ❌ No Error: y is defined inside the function
+  }
+}
+
+2. Invalid Example  // here y is defined outside the function
+function testLet() {
+  if (true) {
+    let y = 20;
+  }
+  console.log(y); // ❌ Error: y is not defined
+}
+------------------------------------------------------------var -----------------------------------
+1. var Variable can be reclared
+var redeclarevar = 10;
+var redeclarevar = 20; // 
+console.log('the value of var redeclarevar as fellows',redeclarevar);  // No Error
+
+//1. Valid Example  // here y1 is defined inside the function
+
+function testVarInside() {
+  if (true) {
+    var y1 = 20;
+      console.log('here var variable called inside the variable', y1); // ❌ No Error: y1 is defined inside the function
+  }
+}
+
+//2. Valid Example  // here y1 is defined outside the function
+function testVarOutside() {
+  if (true) {
+    var y1 = 20;
+  }
+      console.log('here var variable called outside the variable', y1); // ❌ No Error: even y1 is defined outside the function
+}
+
+testVarInside(); testVarOutside();
+
+
+// 📊 Summary Table
+// Feature	        var	                                let	                                const
+// Scope	        Function-scoped	                    Block-scoped	                    Block-scoped
+// Hoisting	    Hoisted + undefined	                Hoisted but TDZ → ReferenceError	Hoisted but TDZ → ReferenceError
+// Redeclaration	Allowed	                            Not allowed	                        Not allowed
+// Reassignment	Allowed	                            Allowed	                            ❌ Not allowed
+// Default Value	undefined	                        undefined	                        Must be initialized
 
 */
 
 
 
 var onlyIntialization;  // we are only initialization the value
-document.write("The Value of only Intialized as follows ", onlyIntialization);  // as no value is initialized then we got output as undefined
+// document.write("The Value of only Intialized as follows ", onlyIntialization);  // as no value is initialized then we got output as undefined
 console.log("Printing the value of only Intialized within the browser console as follows", onlyIntialization);  // as no value is initialized then we got output as undefined
 
 onlyIntialization = 89; // we have assigned new value to onlyIntialization = 89; this is called as declaration 
-document.write("The Value of only Intialization as follows ", onlyIntialization);  // as no value is initialized then we got output as undefined
+// document.write("The Value of only Intialization as follows ", onlyIntialization);  // as no value is initialized then we got output as undefined
 console.log("Printing the value of only Intialization within the browser console as follows", onlyIntialization);  // as no value is initialized then we got output as undefined
 
 
 var intVariable = 45; // this is better way where we have done Intialized & declaration
-document.write("The Value of Variable as follows ", intVariable);
+// document.write("The Value of Variable as follows ", intVariable);
 console.log("Printing the value of int variable within the browser console as follows", intVariable);
 
 var strVariable = "Yogesh";
-document.write("The Value of String Variable as follows ", strVariable);
+// document.write("The Value of String Variable as follows ", strVariable);
 console.log("Printing the value of String Variable within the browser console as follows", strVariable);
 
 var charVariable = 'c';
-document.write("The Value of Char Variable as follows ", charVariable);
+// document.write("The Value of Char Variable as follows ", charVariable);
 console.log("Printing the value of Char Variable within the browser console as follows", charVariable);
 
 var booleanVariable = true;
-document.write("The Value of boolean Variable as follows ", booleanVariable);
+// document.write("The Value of boolean Variable as follows ", booleanVariable);
 console.log("Printing the value of boolean Variable within the browser console as follows", booleanVariable);
 
 // Reassigning the value to existing variable 
 booleanVariable = false;
-document.write("The Value of boolean Variable as follows ", booleanVariable);
+// document.write("The Value of boolean Variable as follows ", booleanVariable);
 console.log("Printing the value of boolean Variable within the browser console as follows", booleanVariable);
 
 // ---- typeof used here -- 
 
-document.write("The Type of Data type as follows ", typeof(booleanVariable));
+// document.write("The Type of Data type as follows ", typeof(booleanVariable));
 console.log("Printing The Type of Data type within the browser console as follows", typeof(booleanVariable));
+
+const fixedValue = 89;
+console.log('The value of const fixedValue as follows', fixedValue);
+// fixedValue = 90; // we have tried to re-assign value to fixedValue as 90
+// console.log('The updated value of const fixedValue as follows', fixedValue);  // it throw error as TypeError: Assignment to constant variable.at Object.<anonymous> (F:\JavaScript Learning\Day2 - Variables And Datatypes\AJavaScriptsVariableAndDatatypes.js:65:12)
+
+// console.log('The value of let noValueVariable as follows', noValueVariable);
+
+
+
+
+
+//------------------------------------------------------- let ---------------------------------------------
+
+let noValueVariable;
+console.log('The value of let noValueVariable as follows', noValueVariable);
+
+
+// let relcarelet = 10;
+// let redelcarelet = 20; // ❌ Error: Identifier 'b' has already been declared
+// console.log(relcarelet);  // The output is SyntaxError: Identifier 'relcarelet' has already been declared
+
+
+
+console.log("redelcare let within the block")
+let c = 10;
+{
+  let c = 20; // ✅ Allowed (it is allowed in block scope)
+  console.log(c); // Output: 20
+}
+console.log(c); // Output: 10
+
+console.log('function test'); // Output: 10
+
+function testLetInside() {
+  if (true) {
+    let y = 20;
+      console.log('here let variable called inside the variable', y); // ❌ No Error: y is defined inside the function
+  }
+}
+
+//2. Invalid Example  // here y is defined outside the function
+// function testLetOutside() {
+//   if (true) {
+//     let y = 20;
+//   }
+//   console.log(y); // ❌ Error: y is not defined
+// }
+
+
+
+//Calling Methods
+testLetInside();
+//testLetOutside();
+
+//------------------------------------------------------- var ---------------------------------------------
+
+//1. Valid Example  // here y1 is defined inside the function
+
+function testVarInside() {
+  if (true) {
+    var y1 = 20;
+      console.log('here var variable called inside the variable', y1); // ❌ No Error: y1 is defined inside the function
+  }
+}
+
+//2. Valid Example  // here y1 is defined outside the function
+function testVarOutside() {
+  if (true) {
+    var y1 = 20;
+  }
+      console.log('here var variable called outside the variable', y1); // ❌ No Error: even y1 is defined outside the function
+}
+
+testVarInside(); testVarOutside();
+
+console.log('redeclare var example')
+var redeclarevar = 10;
+var redeclarevar = 20; // it is allowed in Var
+console.log('the value of var redeclarevar as fellows',redeclarevar);  // No Error
+
--- Day3 - Operators/BAssignmentOperator.js
+++ Day3 - Operators/BAssignmentOperator.js
@@ -3,7 +3,66 @@ console.log('There are different types of operators in JavaScript');
 
 //for Arithmetic operator - please refer AArtihmeticOperators.js
 console.log('1. Arithmetic Operators: It is used to perform Arithmetic Operation such as Sum, subtract, product, Modulus,Divide');
-console.log('2. Assignment Operators');
+console.log('2. Assignment Operators : Assignment Operator is used to assign some value using different symbol =, +=, -=, *=, %=');
+console.log("Assignment Operator")
+
+var a1 = 34; // we are assigning the value 34 to a using = 
+var b1 = 81; //we are assigning the value 81 to a using = 
+var c = a1;  // we are assigning c to a which means the value of a is assigned to c
+
+console.log("Addition and Store within the same Operator")
+var a2 = 56;
+var b2 = 45; 
+a2 += b2; // also known as a1 = a2+ b2;
+console.log("The Output of Addition and Store within the same Operator as follows", a2);
+
+console.log("subtract and Store within the same Operator")
+var a3 = 56;
+var b3 = 45; 
+a3 -= b3; // also known as a3 = a3- b3;
+console.log("The Output of subtract and Store within the same Operator as follows", a3);
+
+console.log("Multiply and Store within the same Operator")
+var a4 = 56;
+var b4 = 45; 
+a4 *= b4; // also known as a4 = a4* b4;
+console.log("The Output of Multiply and Store within the same Operator as follows", a4);
+
+console.log("Divide and Store within the same Operator")
+var a5 = 10;
+var b5 = 2; 
+a5 /= b5; // also known as a5 = a5 /   b5;
+console.log("The Output of Divide and Store within the same Operator as follows", a5);
+
+console.log("Modulus and Store within the same Operator")
+var a6 = 10;
+var b6 = 3; 
+a6 %= b6; // also known as a5 = a6 %   b6;
+console.log("The Output of Modulus and Store within the same Operator as follows", a6);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 console.log('3. Logical Operators');
 console.log('4. Bitwise Operators');
 console.log('5. Ternary Operator');
